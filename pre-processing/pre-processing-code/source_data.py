17b1a823a194abfe04072f90d81cf7c22c26da97
Dara Kharabi <dara.kharabi@rearc.io>
2021-07-22 12:22:37 -0400

    fixed source_data. it now creates CSVs for each sheet.

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -3,14 +3,15 @@ import boto3
 import time
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
-from zipfile import ZipFile
 from s3_md5_compare import md5_compare
-from boto3.s3.transfer import TransferConfig
 from io import BytesIO
+import pandas as pd
 
-def source_dataset(source_dataset_url):
+def source_dataset():
     source_dataset_url = "https://www.ers.usda.gov/webdocs/DataFiles/80591/FoodAccessResearchAtlasData2019.xlsx?v=1848.2"
-    
+    sheets = ["Variable Lookup", "Food Access Research Atlas"]
+
+    print(f'Downloading file at {source_dataset_url}')
     response = None
     retries = 5
     for attempt in range(retries):
@@ -29,39 +30,50 @@ def source_dataset(source_dataset_url):
             
     if response is None:
         raise Exception('There was an issue downloading the dataset')
-            
-    data_set_name = os.environ['DATA_SET_NAME']
+
+    filedata = response.read()
+
+    assets = []
+    asset_list = []
+    data_set_name = os.environ['DATASET_NAME']
 
     data_dir = '/tmp'
     if not os.path.exists(data_dir):
         os.mkdir(data_dir)
 
-    file_location = os.path.join(data_dir, data_set_name+'.csv')
-
-    s3_bucket = os.environ['S3_BUCKET']
+    s3_bucket = os.environ['ASSET_BUCKET']
     s3 = boto3.client('s3')
     s3_resource = boto3.resource('s3')
-    config = TransferConfig(multipart_threshold=1024*25, max_concurrency=10,
-                            multipart_chunksize=1024*25, use_threads=True)
 
     s3_uploads = []
-    asset_list = []
 
-    obj_name = file_location.split('/', 3).pop().replace(' ', '_').lower()
-    file_location = os.path.join(data_dir, obj_name)
-    new_s3_key = data_set_name + '/dataset/' + obj_name
-    filedata = response.read()
+    for sheet_name in sheets:
+        print(f'Sourcing sheet {sheet_name}')
+        xl = pd.ExcelFile(filedata, engine="openpyxl")
 
-    has_changes = md5_compare(s3, s3_bucket, new_s3_key, BytesIO(filedata))
-    if has_changes:
-        s3_resource.Object(s3_bucket, new_s3_key).put(Body=filedata)
-        # sys.exit(0)
-        print('Uploaded: ' + file_location)
-    else:
-        print('No changes in: ' + file_location)
+        if sheet_name not in xl.sheet_names:
+            raise Exception(f"required sheet '{sheet_name}' is missing! Aborting...")
+        
+        sheet_df = xl.parse(sheet_name)
+        file_location = os.path.join(data_dir, sheet_name + '.csv')
 
-    asset_source = {'Bucket': s3_bucket, 'Key': new_s3_key}
-    s3_uploads.append({'has_changes': has_changes, 'asset_source': asset_source})
+        sheet_df.to_csv(file_location)
+
+        obj_name = sheet_name.replace(' ', '_').lower()  + '.csv'
+        new_s3_key = data_set_name + '/dataset/' + obj_name
+
+        with open(file_location, "rb") as fh:
+            has_changes = md5_compare(s3, s3_bucket, new_s3_key, fh)
+
+            if has_changes:
+                s3_resource.Object(s3_bucket, new_s3_key).put(Body=fh.read())
+                # sys.exit(0)
+                print('Uploaded: ' + new_s3_key)
+            else:
+                print('No changes in: ' + new_s3_key)
+
+        asset_source = {'Bucket': s3_bucket, 'Key': new_s3_key}
+        s3_uploads.append({'has_changes': has_changes, 'asset_source': asset_source})
 
     count_updated_data = sum(upload['has_changes'] == True for upload in s3_uploads)
     if count_updated_data > 0:
